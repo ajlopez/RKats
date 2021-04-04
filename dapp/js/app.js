508586249f91d558643170f733516bfd50440371
ajlopez <webmaster@ajlopez.com>
2021-04-04 19:42:36 -0300

    First dapp version, WIP

---
+++ dapp/js/app.js
@@ -0,0 +1,85 @@
+
+const app = (function () {
+    const state = {
+    };
+    
+    const config = {
+        host: 'https://public-node.testnet.rsk.co:443',
+
+        accounts: {
+            alice: '0x4115d23b044e2d5a713329b8cf35520cf7ae8421'
+        },
+        
+        instances: {
+            rkat: '0x128d94b7e7720bfef980533fa1897baf06018dd4'
+        },
+        
+        api: 'http://rskapis.vercel.app/api/testnet/users/'
+    };
+        
+    async function start() {
+        const canvas = document.getElementById('homecanvas');
+
+        const url = config.host;
+        const sender = config.accounts.alice;
+        const contract = config.instances.rkat;
+        
+        const client = rskapi.client(url);
+        
+        try {
+            const data = await client.call(sender, contract, "rkats(uint256)", [ 42 ]);            
+            const id = data.result.substring(0, 12);
+            
+            generateMoonCatImage(id, 16, canvas);            
+        }
+        catch(err) {
+            alert(err);
+        }
+    }
+    
+    let currentPage = 'home';
+    
+    function goTo(newPage) {
+        const newSection = document.getElementById('section_' + newPage);
+        
+        if (!newSection)
+            return;
+        
+        const oldSection = document.getElementById('section_' + currentPage);
+            
+        oldSection.style.display = 'none';
+        newSection.style.display = 'block';
+        
+        currentPage = newPage;
+    }
+    
+    function generateMoonCatImage(catId, size, canvas){
+      size = size || 10;
+      
+      const data = mooncatparser(catId);
+      
+      canvas.width = size * data.length;
+      canvas.height = size * data[1].length;
+      
+      const ctx = canvas.getContext("2d");
+
+      for (let i = 0; i < data.length; i++){
+        for (let j = 0; j < data[i].length; j++){
+          const color = data[i][j];
+          
+          if (color) {
+            ctx.fillStyle = color;
+            ctx.fillRect(i * size, j * size, size, size);
+          }
+        }
+      }
+      
+      return canvas.toDataURL();
+    }
+
+    return {
+        start,
+        goTo
+    }
+})();
+
